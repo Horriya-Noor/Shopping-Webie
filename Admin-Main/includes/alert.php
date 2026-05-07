09351ce3be6ec626e7315a264784167b849f930e
Hoor <64048@students.riphah.edu.pk>
2026-05-07 09:14:40 +0500

    Delete Admin-Main directory
    the folders weren't functioning properly so i'm removing them.

--- Admin-Main/includes/alert.php
+++
@@ -1,6 +0,0 @@
-<?php if ($msg): ?>
-<div class="alert <?= $msg_type ?>" id="main-alert">
-  <span><?= $msg ?></span>
-  <span class="close-x" onclick="document.getElementById('main-alert').remove()">×</span>
-</div>
-<?php endif; ?>
